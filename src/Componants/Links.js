2d9375996cccc04e18784f26d069ecf3267733f7
Shubham7350 <90132010+Shubham7350@users.noreply.github.com>
2022-10-25 13:23:29 +0530

    links corrected

--- src/Componants/Links.js
+++ src/Componants/Links.js
@@ -3,16 +3,16 @@ const Links = () => {
   return (
     <div className="linkDiv">
       <div className="link">
-        <a className="linkName" href="https://www.github..com/Shubham7350">Linkedin</a>
+        <a className="linkName" href="https://www.linkedin.com/in/shubham-ingole">Linkedin</a>
       </div>
       <div className="link">
-        <a className="linkName" href="https://www.github..com/Shubham7350">Twitter</a>
+        <a className="linkName" href="https://www.twitter.com/twtShubz">Twitter</a>
       </div>
       <div className="link">
-        <a className="linkName" href="https://www.github..com/Shubham7350">Github</a>
+        <a className="linkName" href="https://www.github.com/Shubham7350">Github</a>
       </div>
       <div className="link">
-        <a className="linkName" href="https://www.github..com/Shubham7350">Instagram</a>
+        <a className="linkName" href="https://www.instagram.com/ishubham___">Instagram</a>
       </div>
     </div>
   );
