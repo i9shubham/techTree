d99803ac284fac40287355b70e4e355cfae72797
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-10-24 00:32:39 +0530

    add: profile component

--- frontend/src/Pages/UserTree.js
+++ frontend/src/Pages/UserTree.js
@@ -3,13 +3,14 @@ import { fetchData } from '../Services/user';
 import { useLocation } from 'react-router-dom';
 import NotFound from './NotFound';
 import Links from '../Componants/Links';
+import Profile from '../Componants/Profile';
 
 const UserTree = () => {
     const location = useLocation();
     const [data, setData] = useState(null);
-    
+
     const path = location.pathname.slice(1);
-    
+
     useEffect(() => {
         let mounted = true;
         fetchData(path).then((items) => {
@@ -24,7 +25,10 @@ const UserTree = () => {
         <>
             <h1>user</h1>
             {data !== null && typeof data.docs === 'object' ? (
-                <Links link={data.docs?.socials} />
+                <>
+                    <Profile data={data.docs} />
+                    <Links link={data.docs?.socials} />
+                </>
             ) : (
                 <NotFound />
             )}
