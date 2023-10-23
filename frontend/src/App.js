2cbe17acec8f34bd28d10cfdfa148124e40ced9f
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-10-24 00:10:44 +0530

    add: client routes with browserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/Homepage';
-import NotFound from './Pages/NotFound';
 import NewUser from './Pages/NewUser';
-import Shubham from './Pages/Shubham';
+// import Shubham from './Pages/Shubham';
 import Navbar from './Componants/Navbar';
 import Footer from './Componants/Footer';
+import UserTree from './Pages/UserTree';
 
 function App() {
     return (
         <>
             <Navbar />
             <Routes>
-                <Route path='/homepage' element={<NewUser />} />
-                <Route path='/ishubham' element={<Shubham />} />
                 <Route exact path='/' element={<Homepage />} />
-                <Route path='*' element={<NotFound />} />
+                <Route path='/homepage' element={<NewUser />} />
+                <Route path='/*' element={<UserTree />} />
+                {/* <Route path='*' element={<NotFound />} /> */}
             </Routes>
             <Footer />
         </>
