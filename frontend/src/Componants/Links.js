6599d2b2ea66e38a55b46ed19770283fe056d1f4
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-10-24 00:40:04 +0530

    add: Gret text

--- frontend/src/Componants/Links.js
+++ frontend/src/Componants/Links.js
@@ -3,6 +3,7 @@ const Links = (props) => {
     // console.log(props.link);
     return (
         <div className='linkDiv'>
+            <p>Let's Connect</p>
             {props.link.map((data, key) => (
                 <div className='link' key={key}>
                     <a className='linkName' href={data.link}>
