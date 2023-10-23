4e9bcf3cfc1a742f720256f9026298683426e1ec
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-10-24 00:07:42 +0530

    add: server logs

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoConnection from './config/dbconfig.js';
+import logger from 'morgan';
+
 import userRoutes from './routes/user.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(logger('dev'));
 mongoConnection();
 app.use(express.json());
 app.use(cors());
