dc89f5e567e1baab6d877aaf2bbc63d249fb4c92
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-10-26 20:23:55 +0530

    add: homepage data

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container } from '@mui/material';
-import UserCard from '../Componants/Homepage/UserCard';
 import Tree from '../Images/tree-vector.jpg';
-import './index.css'
+import './index.css';
 const Homepage = () => {
+    const navigate = useNavigate();
+    const handleTreeInputClick = () => {
+        navigate('/createTree');
+    };
     return (
         <div className='page'>
             <Container
@@ -15,6 +19,9 @@ const Homepage = () => {
                     // alignItems: 'center',
                 }}
             >
+                <div style={{ textAlign: 'center' }}>
+                    <img className='tree' src={Tree} alt='TechTree' />
+                </div>
                 <div>
                     <h1>Everything you are. In one, simple link in bio.</h1>
                     <p>
@@ -24,16 +31,24 @@ const Homepage = () => {
                         deserunt quidem, delectus voluptatem. Commodi quam cum
                         esse quisquam?
                     </p>
+                    <br />
                 </div>
-                <div style={{textAlign: 'center'}}>
-                    <img className='tree' src={Tree} alt='TechTree' />
-                    <p style={{ }}>To the roots</p>
-                </div>
-                <div style={{textAlign: 'center', margin: '0, auto'}}>
+
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <div className='create-link-comp'>
-                        techtree.com/<span style={{fontWeight: 'bold'}}>yourname</span>
+                        <button
+                            className='treeinput'
+                            onClick={handleTreeInputClick}
+                        >
+                            <span style={{ opacity: 0.8 }}>techtree.com/</span>
+                            <span style={{ fontWeight: 'bold', opacity: 1 }}>
+                                yourname
+                            </span>
+                        </button>
                     </div>
                 </div>
+
+                <p style={{ lineHeight: 1.8 }}>Find your Roots</p>
             </Container>
         </div>
     );
