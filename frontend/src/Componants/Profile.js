f39919d91e2cafe3d18b588b51735d292ad2c619
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-11-02 01:19:37 +0530

    add: prop drilling for profile data

--- frontend/src/Componants/Profile.js
+++ frontend/src/Componants/Profile.js
@@ -1,22 +1,26 @@
 import './Profile.css';
-import myImage from '../Images/ishubham.png';
-import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
-import LoyaltyIcon from '@mui/icons-material/Loyalty';
-import { Container } from '@mui/material';
-const Profile = () => {
+// import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+// import LoyaltyIcon from '@mui/icons-material/Loyalty';
+const Profile = (props) => {
+    console.log(props.data.image);
     return (
         <>
             <div className='profileDiv'>
-                <img src={myImage} alt='Profile Pic' className='profile-pic' />
+                <img
+                    src={`data:image/jpeg;base64,${props.data.image}`}
+                    alt='Profile Pic'
+                    className='profile-pic'
+                />
             </div>
-            <div className='tools'>
+            {/* <div className='tools'>
                 <LoyaltyIcon />
                 <QrCodeScannerIcon />
-            </div>
-            <h1 className='profileDiv'> Mr. Shubham Ingole</h1>
-            <p className='profileDiv'>
-                Full-Stack Developer | CP-C++ | Graphics | CSE'24
+            </div> */}
+            <h1 className='profileDiv'> {props.data.name}</h1>
+            <p>
+                <a href={`mailto:${props.data.email}`}>{props.data.email}</a>
             </p>
+            <p className='profileDiv'>{props.data.bio}</p>
         </>
     );
 };
