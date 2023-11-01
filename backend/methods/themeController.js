d47837b0d6a5ef96c682f1663fcf501a7ecaa66d
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-11-02 00:49:52 +0530

    add: create theme api

--- backend/methods/themeController.js
+++ backend/methods/themeController.js
@@ -26,3 +26,31 @@ export const getTheme = async (req, res) => {
         });
     }
 };
+
+export const addTheme = async (req, res) => {
+    try {
+        const data = new themeModel(req.body);
+        const response = await data.save();
+        if (!response) {
+            res.status(400).send({
+                code: 400,
+                message: 'There is no such theme',
+                success: false,
+            });
+        } else {
+            res.status(200).send({
+                code: 200,
+                meesage: 'Theme getting successfully',
+                success: true,
+                data: data,
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            code: 500,
+            message: 'Internal Server Error',
+            success: false,
+        });
+    }
+};
