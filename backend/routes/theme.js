e6d69d7dc283199a0ae2ef2acc0459e25b336446
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-11-02 01:03:16 +0530

    add: create theme route

--- backend/routes/theme.js
+++ backend/routes/theme.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getTheme } from '../methods/themeController.js';
+import { addTheme, getTheme } from '../methods/themeController.js';
 
 const router = express.Router();
 
 router.get('/getTheme/:id', getTheme);
+router.post('/createTheme', addTheme);
 
 export default router;
