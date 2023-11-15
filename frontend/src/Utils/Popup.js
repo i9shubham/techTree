2603bea357334512fe5120020504cf1268a2f97c
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-11-15 13:12:03 +0530

    add: popup util component

---
+++ frontend/src/Utils/Popup.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Error = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    padding: 2rem;
+    height: 100%;
+    border-radius: 4px;
+    background-color: #ffffff50;
+    color: #000;
+    font-weight: bold;
+`;
+
+const Popup = (props) => {
+    console.log(props.error);
+    // const [error, setError] = useState(props.error);
+    // useEffect(() => {
+    if (props.error !== null) {
+        setTimeout(() => {
+            props.setError();
+        }, 3000);
+    }
+    // }, [props]);
+    return (
+        <div>
+            {props.error !== null && (
+                <Error>
+                    <p style={{ color: '#000' }}>{props.error}</p>
+                </Error>
+            )}
+        </div>
+    );
+};
+
+export default Popup;
