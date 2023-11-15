39194bd61a33a7b5b8033b8c1e5d02a100d6a348
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-11-15 20:28:16 +0530

    add: routes and initiate local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/Homepage';
@@ -6,18 +7,32 @@ import NewUser from './Pages/NewUser';
 import Navbar from './Componants/Navbar';
 import Footer from './Componants/Footer';
 import UserTree from './Pages/UserTree';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    margin: 5px auto 5px auto;
+`;
 
 function App() {
+    useEffect(() => {
+        if (!window.localStorage.getItem('user')) {
+            window.localStorage.setItem('user', null);
+            window.localStorage.setItem('signup', 'false');
+            window.localStorage.setItem('theme', null);
+        }
+    }, []);
     return (
         <>
-            <Navbar />
-            <Routes>
-                <Route exact path='/' element={<Homepage />} />
-                <Route path='/homepage' element={<NewUser />} />
-                <Route path='/*' element={<UserTree />} />
-                {/* <Route path='*' element={<NotFound />} /> */}
-            </Routes>
-            <Footer />
+            <Container style={{ fontFamily: 'seoge ui' }}>
+                <Navbar />
+                <Routes>
+                    <Route exact path='/' element={<Homepage />} />
+                    <Route path='/signup' element={<NewUser />} />
+                    <Route path='/*' element={<UserTree />} />
+                    {/* <Route path='*' element={<NotFound />} /> */}
+                </Routes>
+                <Footer />
+            </Container>
         </>
     );
 }
