042e1c1cd6492af876e8704a740318c55292130f
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-11-15 20:04:28 +0530

    add: user api calls

--- frontend/src/Services/user.js
+++ frontend/src/Services/user.js
@@ -7,3 +7,19 @@ export const fetchData = async (path) => {
         responseType: 'json',
     });
 };
+
+export const findUserName = async (username) => {
+    return await axios({
+        url: `/user/checkUserName/${username}`,
+        method: 'GET',
+        // responseType: 'json',
+    });
+};
+
+export const registerUser = async (details) => {
+    return await axios({
+        url: `/user/signup`,
+        method: 'POST',
+        data: details,
+    });
+};
