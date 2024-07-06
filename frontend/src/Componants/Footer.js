ebb0a571a0b67c807a4c959cb8c67673da9f8b34
Shubhahm Ingole <ishubham158@gmail.com>
2024-07-06 21:52:09 +0530

    fix: footer

--- frontend/src/Componants/Footer.js
+++ frontend/src/Componants/Footer.js
@@ -1,9 +1,26 @@
-import "./Footer.css";
+import './Footer.css';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+
 const Footer = () => {
-  return (
-    <div className="footer">
-      <p>©️2022 Shubham Ingole</p>
-    </div>
-  );
+    return (
+        <div className='footer'>
+            <div className='footer-content'>
+                <p>©️2022 Shubham Ingole</p>
+                <div className='footer-links'>
+                    <a href='https://www.linkedin.com/in/i9shubham/'>
+                        <FaLinkedin />
+                    </a>
+                    <a href='https://www.github.com/i9shubham/'>
+                        <FaGithub />
+                    </a>
+                    <a href='https://twitter.com/i9shubham'>
+                        <FaXTwitter />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default Footer;
