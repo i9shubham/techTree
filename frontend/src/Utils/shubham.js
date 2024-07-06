96a2599c64713bb1d8984cc7b162b31be1eb3e83
Shubhahm Ingole <ishubham158@gmail.com>
2024-07-06 21:59:46 +0530

    add: Shubham Data

---
+++ frontend/src/Utils/shubham.js
@@ -0,0 +1,32 @@
+export const data = {
+    name: 'Shubham Ingole',
+    username: 'i9shubaham',
+    email: 'ershubhamingole@gmail.com',
+    bio: 'I am a full stack developer.',
+    links: [
+        {
+            title: 'Portfolio',
+            url: 'https://shubhamingole.netlify.app/',
+        },
+        {
+            title: 'GitHub',
+            url: 'https://github.com/i9shubham',
+        },
+        {
+            title: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/i9shubham/',
+        },
+        {
+            title: 'Twitter',
+            url: 'https://twitter.com/i9shubham',
+        },
+        {
+            title: 'Instagram',
+            url: 'https://www.instagram.com/i9shubham/',
+        },
+        {
+            title: 'Blog',
+            url: 'https://i9shubham.hashnode.dev/',
+        },
+    ],
+};
